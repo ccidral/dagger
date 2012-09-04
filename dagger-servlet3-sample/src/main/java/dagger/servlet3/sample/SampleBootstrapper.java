955e72d6140ea3dc7bbb99de140b01611b656e85
Celio Cidral Junior <celio.cidral@rtsbrasil.com.br>
2012-09-04 07:52:29 -0300

    Created sample web project.

---
+++ dagger-servlet3-sample/src/main/java/dagger/servlet3/sample/SampleBootstrapper.java
@@ -0,0 +1,23 @@
+package dagger.servlet3.sample;
+
+import dagger.Action;
+import dagger.RequestHandlers;
+import dagger.Result;
+import dagger.handlers.Get;
+import dagger.resource.ExactResourceName;
+import dagger.results.Ok;
+import dagger.servlet3.Bootstrapper;
+
+public class SampleBootstrapper implements Bootstrapper {
+
+    @Override
+    public void bootstrap(RequestHandlers requestHandlers) {
+         requestHandlers.add(new Get(new ExactResourceName("/"), new Action() {
+             @Override
+             public Result execute() {
+                 return new Ok("Home");
+             }
+         }));
+    }
+
+}
