7b0e0542afcda0a8e2efadc059c277c64ca9ea83
Celio Cidral Junior <celio.cidral@rtsbrasil.com.br>
2012-10-16 21:34:09 -0300

    Replacing the current directory watcher library by Java 7's WatchService.

--- dagger-server-netty-dev/src/main/java/dagger/server/netty/DirectoryWatcher.java
+++ dagger-server-netty-dev/src/main/java/dagger/server/netty/DirectoryWatcher.java
@@ -1,83 +1,44 @@
 package dagger.server.netty;
 
-import net.contentobjects.jnotify.JNotify;
-import net.contentobjects.jnotify.JNotifyListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
+import java.nio.file.*;
+import java.util.List;
+
+import static java.nio.file.StandardWatchEventKinds.*;
 
 public class DirectoryWatcher {
 
-    private final int watchId;
-    private final Object monitor = new Object();
-
-    private long lastChangeTime;
-
+    private final WatchKey key;
+    private final WatchService watchService;
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     public DirectoryWatcher(String directory) throws Throwable {
-        watchId = JNotify.addWatch(directory, JNotify.FILE_ANY, true, new Listener());
+        Path dir = FileSystems.getDefault().getPath(directory);
+
+        watchService = FileSystems.getDefault().newWatchService();
+        key = dir.register(watchService,
+                ENTRY_CREATE,
+                ENTRY_DELETE,
+                ENTRY_MODIFY);
     }
 
     public void stopWatching() throws Throwable {
-        JNotify.removeWatch(watchId);
+        watchService.close();
     }
 
     public void waitForChange() throws InterruptedException {
-        lastChangeTime = 0;
-        do {
-            long timeToWait = timeToWait();
+        while(true) {
+            Thread.sleep(1000);
 
-            logger.debug("Will wait " + (timeToWait < Long.MAX_VALUE ? timeToWait + " millis" : "a lot"));
+            List<WatchEvent<?>> event = key.pollEvents();
 
-            if(timeToWait > 0)
-                synchronized (monitor) {
-                    monitor.wait(timeToWait);
-                }
-        } while(changedLessThanOneSecondAgo());
+            key.reset();
 
-        logger.info("Age is " + age());
-    }
-
-    private long timeToWait() {
-        return lastChangeTime == 0 ? Long.MAX_VALUE : 1000L - age();
-    }
-
-    private long age() {
-        return System.currentTimeMillis() - lastChangeTime;
-    }
-
-    private boolean changedLessThanOneSecondAgo() {
-        return age() > 0 && age() < 1000L;
-    }
-
-    private void markAsChanged(File file) {
-        lastChangeTime = System.currentTimeMillis();
-        logger.debug("Changed {}", file);
-        synchronized (monitor) {
-            monitor.notifyAll();
+            if(event.size() > 0)
+                break;
         }
-    }
-
-    private class Listener implements JNotifyListener {
-
-        public void fileRenamed(int wd, String rootPath, String oldName, String newName) {
-            markAsChanged(new File(rootPath, oldName));
-        }
-
-        public void fileModified(int wd, String rootPath, String name) {
-            markAsChanged(new File(rootPath, name));
-        }
-
-        public void fileDeleted(int wd, String rootPath, String name) {
-            markAsChanged(new File(rootPath, name));
-        }
-
-        public void fileCreated(int wd, String rootPath, String name) {
-            markAsChanged(new File(rootPath, name));
-        }
-
     }
 
 }
