714285dec3b4b2da02d1db21e1a8848b309bfc7c
Célio Cidral Junior <ccidral@gmail.com>
2012-11-27 11:30:01 -0200

    Added two new HTTP header names.

--- dagger-core/src/main/java/dagger/http/HttpHeaderNames.java
+++ dagger-core/src/main/java/dagger/http/HttpHeaderNames.java
@@ -2,8 +2,10 @@ package dagger.http;
 
 public class HttpHeaderNames {
 
+    public static final String CACHE_CONTROL = "Cache-Control";
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String DATE = "Date";
+    public static final String EXPIRES = "Expires";
     public static final String LAST_MODIFIED = "Last-Modified";
     public static final String LOCATION = "Location";
     public static final String IF_MODIFIED_SINCE = "If-Modified-Since";
