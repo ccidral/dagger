9a55a1a92c9555aaf1d374baaf6899d747d3a0df
Célio Cidral Junior <ccidral@gmail.com>
2013-09-15 23:01:42 -0300

    Minor refactoring.

--- dagger-core/src/test/java/dagger/routes/ExactRouteTest.java
+++ dagger-core/src/test/java/dagger/routes/ExactRouteTest.java
@@ -9,9 +9,14 @@ import static junit.framework.Assert.assertTrue;
 public class ExactRouteTest {
 
     @Test
-    public void testMatches() {
+    public void test_does_match_exact_uri() {
         Route route = new ExactRoute("/foo");
         assertTrue(route.matches("/foo"));
+    }
+
+    @Test
+    public void test_does_not_match_a_different_uri() {
+        Route route = new ExactRoute("/foo");
         assertFalse(route.matches("/bar"));
     }
 
