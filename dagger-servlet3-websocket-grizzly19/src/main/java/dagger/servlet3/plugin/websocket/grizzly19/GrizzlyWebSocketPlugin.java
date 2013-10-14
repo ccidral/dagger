90e5fce681df9b2a7ef6cd3d8806bde3f6167bd6
Célio Cidral Junior <ccidral@gmail.com>
2013-10-14 13:40:22 -0300

    Initialize the websocket application.

--- dagger-servlet3-websocket-grizzly19/src/main/java/dagger/servlet3/plugin/websocket/grizzly19/GrizzlyWebSocketPlugin.java
+++ dagger-servlet3-websocket-grizzly19/src/main/java/dagger/servlet3/plugin/websocket/grizzly19/GrizzlyWebSocketPlugin.java
@@ -2,6 +2,10 @@ package dagger.servlet3.plugin.websocket.grizzly19;
 
 import com.sun.grizzly.websockets.WebSocketEngine;
 import dagger.Module;
+import dagger.http.DefaultQueryStringParser;
+import dagger.http.QueryStringParser;
+import dagger.servlet3.lang.ServletUriParser;
+import dagger.servlet3.lang.ServletUriParserImpl;
 import dagger.servlet3.plugin.DaggerServletPlugin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,8 +20,13 @@ public class GrizzlyWebSocketPlugin implements DaggerServletPlugin {
     public void initialize(Module module, ServletConfig servletConfig) {
         logger.info("Creating websocket app");
 
-        //TODO
-        //webSocketApp = new DaggerWebSocketApp(module, requestConversor);
+        ServletUriParser servletUriParser = new ServletUriParserImpl();
+        QueryStringParser queryStringParser = new DefaultQueryStringParser();
+        RequestFactory requestFactory = new DefaultRequestFactory(servletUriParser, queryStringParser);
+        WebSocketFactory webSocketFactory = new WebSocketFactoryImpl();
+        ResponseFactory responseFactory = new DefaultResponseFactory();
+        WebSocketListenerFactory webSocketListenerFactory = new WebSocketListenerFactoryImpl(requestFactory, responseFactory);
+        webSocketApp = new DaggerWebSocketApp(module, requestFactory, webSocketFactory, webSocketListenerFactory);
 
         logger.info("Registering websocket app");
         WebSocketEngine.getEngine().register(webSocketApp);
