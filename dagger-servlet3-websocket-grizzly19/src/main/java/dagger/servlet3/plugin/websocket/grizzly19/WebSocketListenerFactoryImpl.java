d890819ca78b34c37e33655b12cad116c9ebb06f
Célio Cidral Junior <ccidral@gmail.com>
2013-10-14 11:45:54 -0300

    Implement WebSocketListenerFactory.

--- dagger-servlet3-websocket-grizzly19/src/main/java/dagger/servlet3/plugin/websocket/grizzly19/WebSocketListenerFactoryImpl.java
+++ dagger-servlet3-websocket-grizzly19/src/main/java/dagger/servlet3/plugin/websocket/grizzly19/WebSocketListenerFactoryImpl.java
@@ -2,15 +2,22 @@ package dagger.servlet3.plugin.websocket.grizzly19;
 
 import com.sun.grizzly.websockets.WebSocketListener;
 import dagger.Module;
-import dagger.lang.NotImplementedYet;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class WebSocketListenerFactoryImpl implements WebSocketListenerFactory {
 
+    private final RequestFactory requestFactory;
+    private final ResponseFactory responseFactory;
+
+    public WebSocketListenerFactoryImpl(RequestFactory requestFactory, ResponseFactory responseFactory) {
+        this.requestFactory = requestFactory;
+        this.responseFactory = responseFactory;
+    }
+
     @Override
-    public WebSocketListener create(Module module, HttpServletRequest request) {
-        throw new NotImplementedYet();
+    public WebSocketListener create(Module module, HttpServletRequest httpServletRequest) {
+        return new DaggerWebSocketListener(module, httpServletRequest, requestFactory, responseFactory);
     }
 
 }
