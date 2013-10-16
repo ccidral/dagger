c4aa437273bfbd31d666313796a460e4aeb24a3a
Célio Cidral Junior <ccidral@gmail.com>
2013-10-16 08:41:04 -0300

    Add javadoc that refers to Reroute when there is need to redirect to a location under the context path.

--- dagger-core/src/main/java/dagger/actions/Redirect.java
+++ dagger-core/src/main/java/dagger/actions/Redirect.java
@@ -5,6 +5,14 @@ import dagger.Reaction;
 import dagger.http.Request;
 import dagger.reactions.Redirection;
 
+/**
+ * Redirects to a new location.
+ *
+ * <p>
+ *     If you need to redirect to a new location under the
+ *     context path, see {@link Reroute}.
+ * </p>
+ */
 public class Redirect implements Action {
 
     private final String location;
