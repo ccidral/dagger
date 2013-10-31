b8d294b15f3294db3f3f088fb37a19b760999c9e
Célio Cidral Junior <ccidral@gmail.com>
2013-10-31 08:49:26 -0200

    Define constants for some common statuses.

--- dagger-core/src/main/java/dagger/reactions/Status.java
+++ dagger-core/src/main/java/dagger/reactions/Status.java
@@ -11,6 +11,10 @@ import java.io.Writer;
 
 public class Status implements Reaction {
 
+    public static final Reaction BAD_REQUEST = new Status(StatusCode.BAD_REQUEST, "Bad request");
+    public static final Reaction NOT_FOUND = new Status(StatusCode.NOT_FOUND, "Not found");
+    public static final Reaction INTERNAL_SERVER_ERROR = new Status(StatusCode.INTERNAL_SERVER_ERROR, "Internal server error");
+
     private final StatusCode statusCode;
     private final String message;
 
