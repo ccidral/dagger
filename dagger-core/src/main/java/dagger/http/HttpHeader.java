58deb5ccd7a61d42f5e061d0b943057d82488adc
Célio Cidral Junior <ccidral@gmail.com>
2013-10-31 16:26:20 -0200

    Add CONTENT_DISPOSITION header name constant.

--- dagger-core/src/main/java/dagger/http/HttpHeader.java
+++ dagger-core/src/main/java/dagger/http/HttpHeader.java
@@ -3,6 +3,7 @@ package dagger.http;
 public class HttpHeader {
 
     public static final String CACHE_CONTROL = "Cache-Control";
+    public static final String CONTENT_DISPOSITION = "Content-Disposition";
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String DATE = "Date";
     public static final String EXPIRES = "Expires";
