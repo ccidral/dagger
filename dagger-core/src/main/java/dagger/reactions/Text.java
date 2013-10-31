bfc4563ffef8f420e98ea4790acac65d9e9fd001
Célio Cidral Junior <ccidral@gmail.com>
2013-10-31 10:33:43 -0200

    Introducing the Text reaction.

---
+++ dagger-core/src/main/java/dagger/reactions/Text.java
@@ -0,0 +1,48 @@
+package dagger.reactions;
+
+import dagger.Reaction;
+import dagger.http.HttpHeaderNames;
+import dagger.http.Request;
+import dagger.http.Response;
+import dagger.http.StatusCode;
+
+import java.io.*;
+
+public class Text implements Reaction {
+
+    private static final int READ_BUFFER_SIZE = 2048;
+
+    private final Reader reader;
+    private final String contentType;
+
+    public Text(String text, String contentType) {
+        this(new StringReader(text), contentType);
+    }
+
+    public Text(InputStream inputStream, String contentType) {
+        this(new InputStreamReader(inputStream), contentType);
+    }
+
+    public Text(Reader reader, String contentType) {
+        this.reader = reader;
+        this.contentType = contentType;
+    }
+
+    @Override
+    public void execute(Request request, Response response) throws Exception {
+        response.setStatusCode(StatusCode.OK);
+        response.setHeader(HttpHeaderNames.CONTENT_TYPE, contentType);
+        writeTextTo(response);
+    }
+
+    private void writeTextTo(Response response) throws IOException {
+        Writer writer = new OutputStreamWriter(response.getOutputStream());
+        char[] buffer = new char[READ_BUFFER_SIZE];
+        int charsRead;
+        while((charsRead = reader.read(buffer)) > -1) {
+            writer.write(buffer, 0, charsRead);
+        }
+        writer.flush();
+    }
+
+}
