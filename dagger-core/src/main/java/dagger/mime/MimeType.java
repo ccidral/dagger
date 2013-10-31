a0e1f43c9b7d042463cf6dab8ae3e85fe1e00429
Célio Cidral Junior <ccidral@gmail.com>
2013-10-31 16:27:07 -0200

    Add MIME type constant for 'application/pdf'.

--- dagger-core/src/main/java/dagger/mime/MimeType.java
+++ dagger-core/src/main/java/dagger/mime/MimeType.java
@@ -5,6 +5,7 @@ public class MimeType {
     public static final String APPLICATION_JAVASCRIPT = "application/javascript";
     public static final String APPLICATION_JSON = "application/json";
     public static final String APPLICATION_OCTET_STREAM = "application/octet-stream";
+    public static final String APPLICATION_PDF = "application/pdf";
     public static final String IMAGE_GIF = "image/gif";
     public static final String IMAGE_JPEG = "image/jpeg";
     public static final String IMAGE_PNG = "image/png";
