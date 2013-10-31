f3098b53b2e17d02f083d5d3da5b0813ffd153fd
Célio Cidral Junior <ccidral@gmail.com>
2013-10-31 10:03:08 -0200

    Add reaction that writes binary content to the HTTP response.

---
+++ dagger-core/src/main/java/dagger/reactions/Binary.java
@@ -0,0 +1,46 @@
+package dagger.reactions;
+
+import dagger.Reaction;
+import dagger.http.HttpHeaderNames;
+import dagger.http.Request;
+import dagger.http.Response;
+import dagger.http.StatusCode;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Binary implements Reaction {
+
+    private static final int READ_BUFFER_SIZE = 2048;
+
+    private final InputStream inputStream;
+    private final String contentType;
+
+    public Binary(byte[] bytes, String contentType) {
+        this(new ByteArrayInputStream(bytes), contentType);
+    }
+
+    public Binary(InputStream inputStream, String contentType) {
+        this.inputStream = inputStream;
+        this.contentType = contentType;
+    }
+
+    @Override
+    public void execute(Request request, Response response) throws Exception {
+        response.setStatusCode(StatusCode.OK);
+        response.setHeader(HttpHeaderNames.CONTENT_TYPE, contentType);
+        writeBytesTo(response);
+    }
+
+    private void writeBytesTo(Response response) throws IOException {
+        OutputStream outputStream = response.getOutputStream();
+        byte[] buffer = new byte[READ_BUFFER_SIZE];
+        int bytesRead;
+        while((bytesRead = inputStream.read(buffer)) > -1) {
+            outputStream.write(buffer, 0, bytesRead);
+        }
+    }
+
+}
---
+++ dagger-core/src/test/java/dagger/reactions/BinaryTest.java
@@ -0,0 +1,67 @@
+package dagger.reactions;
+
+import dagger.Reaction;
+import dagger.http.HttpHeaderNames;
+import dagger.http.Response;
+import dagger.http.StatusCode;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class BinaryTest {
+
+    private Response response;
+
+    @Before
+    public void setUp() throws Exception {
+        response = mock(Response.class);
+        when(response.getOutputStream()).thenReturn(new ByteArrayOutputStream());
+    }
+
+    private byte[] bytesWrittenTo(Response response) {
+        ByteArrayOutputStream outputStream = (ByteArrayOutputStream) response.getOutputStream();
+        return outputStream.toByteArray();
+    }
+
+    @Test
+    public void test_status_code_is_200_OK() throws Throwable {
+        InputStream inputStream = new ByteArrayInputStream(new byte[]{1, 2, 3});
+        Reaction reaction = new Binary(inputStream, "image/png");
+        reaction.execute(null, response);
+        verify(response).setStatusCode(StatusCode.OK);
+    }
+
+    @Test
+    public void test_set_content_type_header() throws Throwable {
+        InputStream inputStream = new ByteArrayInputStream(new byte[]{1, 2, 3});
+        Reaction reaction = new Binary(inputStream, "image/png");
+        reaction.execute(null, response);
+        verify(response).setHeader(HttpHeaderNames.CONTENT_TYPE, "image/png");
+    }
+
+    @Test
+    public void test_write_bytes_from_input_stream_to_response() throws Throwable {
+        byte[] theBytes = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        InputStream inputStream = new ByteArrayInputStream(theBytes);
+        Reaction reaction = new Binary(inputStream, "image/png");
+        reaction.execute(null, response);
+        assertArrayEquals(theBytes, bytesWrittenTo(response));
+    }
+
+    @Test
+    public void test_write_bytes_to_response() throws Throwable {
+        byte[] theBytes = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Reaction reaction = new Binary(theBytes, "image/png");
+        reaction.execute(null, response);
+        assertArrayEquals(theBytes, bytesWrittenTo(response));
+    }
+
+}
