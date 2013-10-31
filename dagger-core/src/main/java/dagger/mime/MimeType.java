da100cfa7326befca2a3a52342c0843dd8b13260
Célio Cidral Junior <ccidral@gmail.com>
2013-10-31 10:32:13 -0200

    Add some MIME type constants.

---
+++ dagger-core/src/main/java/dagger/mime/MimeType.java
@@ -0,0 +1,9 @@
+package dagger.mime;
+
+public class MimeType {
+
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String TEXT_HTML = "text/html";
+    public static final String TEXT_PLAIN = "text/plain";
+
+}
