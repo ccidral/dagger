3baf93c85e017a11433d3202d096166a923709a1
Célio Cidral Junior <ccidral@gmail.com>
2013-11-25 15:48:13 -0200

    Added a few more predefined status code reactions.

--- dagger-core/src/main/java/dagger/reactions/Status.java
+++ dagger-core/src/main/java/dagger/reactions/Status.java
@@ -12,9 +12,13 @@ import java.io.Writer;
 public class Status implements Reaction {
 
     public static final Reaction BAD_REQUEST = new Status(StatusCode.BAD_REQUEST, "Bad request");
+    public static final Reaction FORBIDDEN = new Status(StatusCode.INTERNAL_SERVER_ERROR, "Forbidden");
     public static final Reaction INTERNAL_SERVER_ERROR = new Status(StatusCode.INTERNAL_SERVER_ERROR, "Internal server error");
+    public static final Reaction METHOD_NOT_ALLOWED = new Status(StatusCode.NO_CONTENT, "Method not allowed");
     public static final Reaction NO_CONTENT = new Status(StatusCode.NO_CONTENT, "No content");
     public static final Reaction NOT_FOUND = new Status(StatusCode.NOT_FOUND, "Not found");
+    public static final Reaction SERVICE_UNAVAILABLE = new Status(StatusCode.SERVICE_UNAVAILABLE, "Service unavailable");
+    public static final Reaction UNAUTHORIZED = new Status(StatusCode.SERVICE_UNAVAILABLE, "Unauthorized");
 
     private final StatusCode statusCode;
     private final String message;
